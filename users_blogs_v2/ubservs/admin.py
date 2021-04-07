88a7962bfb16448ce48a42ca5833d225f3d0138b
Pavel <popovpp@yandex.ru>
2021-04-07 22:53:40 +0300

    have made models

--- users_blogs_v2/ubservs/admin.py
+++ users_blogs_v2/ubservs/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Post, Blog
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'show_blogs_suscribe', 
+                    'show_read_posts']
+    list_display_links = ['username']
+    search_fields = ['username', 'email']
+
+    class Meta:
+        model = User
+
+    def show_blogs_suscribe(self, obj):
+        return ", ".join([a.author for a in obj.blogs_suscribe.all()])
+
+    def show_read_posts(self, obj):
+        return ", ".join([a.id for a in obj.read_posts.all()])
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'author', 'timestamp']
+    list_display_links = ['title']
+    search_fields = ['title', 'author']
+
+    class Meta:
+        model = Post
+
+
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'show_posts']
+    list_display_links = ['id']
+    search_fields = ['author']
+
+    class Meta:
+        model = Blog
+
+    def show_posts(self, obj):
+        return ", ".join([a.id for a in obj.posts.all()])
+
+
+admin.site.register(User, UserModelAdmin)
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Blog, BlogModelAdmin)
--- users_blogs_v2/ubservs/models.py
+++ users_blogs_v2/ubservs/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    blogs_suscribe = models.ManyToManyField('Blog', blank=True,
+                                   verbose_name='blogs', null=True)
+    read_posts = models.ManyToManyField('Post', blank=True,
+                                   verbose_name='posts', null=True)
+    def __unicode__(self):
+        return self.username
+
+    def __str__(self):
+        return self.username
+
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='Post id')
+    title = models.CharField(max_length=255, verbose_name='Заголовок поста')
+    text = models.TextField(verbose_name='Текст поста')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               verbose_name='Автор поста')
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True, 
+                                     verbose_name='Дата создания поста')
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta():
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        db_table = 'posts'
+        ordering = ['-id']
+
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='Blog id')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               verbose_name='Автор блога')
+    posts = models.ManyToManyField(Post, blank=True,
+                                   verbose_name='posts', null=True)
+    
+    def __unicode__(self):
+        return self.id
+
+    def __str__(self):
+        return self.id
+
+    class Meta():
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
+        db_table = 'blogs'
+        ordering = ['-id']
